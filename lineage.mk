ecea02a771567e7c08e1c57e3653335f4e121cdb
scoty755 <scoty755@gmail.com>
2017-01-29 00:51:55 +0900

    b3uhl: rebrand to LineageOS

--- lineage.mk
+++ lineage.mk
@@ -1,9 +1,9 @@
 $(call inherit-product, device/htc/b3uhl/full_b3uhl.mk)
 
-# Inherit some common CM stuff.
+# Inherit some common Lineage stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-PRODUCT_NAME := cm_b3uhl
+PRODUCT_NAME := lineage_b3uhl
 BOARD_VENDOR := htc
 TARGET_VENDOR := htc
 PRODUCT_DEVICE := b3uhl
